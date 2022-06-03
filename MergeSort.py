2ad64f43ddac29928d4af06556418b591354310b
JeannieChen <jeannie_chen@hotmail.com>
2022-06-03 12:45:03 -0400

    merge sort

---
+++ MergeSort.py
@@ -0,0 +1,34 @@
+def merge(list1, list2):
+    combined = []
+
+    i, j = 0, 0
+    while i < len(list1) and j < len(list2):
+        if list1[i] < list2[j]:
+            combined.append(list1[i])
+            i += 1
+        else:
+            combined.append(list2[j])
+            j += 1
+    while i < len(list1):
+        combined.append(list1[i])
+        i += 1
+    while j < len(list2):
+        combined.append(list2[j])
+        j += 1
+
+    return combined
+
+def merge_sort(my_list):
+    if len(my_list) == 1:
+        return my_list
+    mid = int(len(my_list)/2)
+    left = my_list[:mid]
+    right = my_list[mid:]
+    return merge(merge_sort(left), merge_sort(right))
+
+#Test
+li1 = [2,5,6,9]
+li2 = [1,3,5,8,9]
+print(merge(li1, li2))
+
+print(merge_sort([1,6,5,8,4,3]))
